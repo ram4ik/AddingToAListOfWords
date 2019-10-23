3e9ee9077ae6013cb0d4bc622351650500a5377e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-23 11:03:59 +0300

    init

--- AddingToAListOfWords/ContentView.swift
+++ AddingToAListOfWords/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                List(userWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                    
+                }
+            }
+        .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        // extra validation to come
+        
+        userWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
